030fa0ff9bb43341526a5f50beede41c6b724f1f
yaprincev <5489812@mail.ru>
2024-02-09 23:10:01 +0300

    init

---
+++ ColorApp/ColorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorApp
+//
+//  Created by Максим Япринцев on 09.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
